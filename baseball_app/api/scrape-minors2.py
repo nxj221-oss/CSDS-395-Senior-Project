6077976201cc1dbe701632d544a488e730d54053
Ari Glockner <ari@glockners.net>
2025-12-01 16:55:54 -0500

    Updated format of scrape-minors2

--- baseball_app/api/scrape-minors2.py
+++ baseball_app/api/scrape-minors2.py
@@ -23,9 +23,42 @@ import pandas as pd
 from io import StringIO
 import sys
 
-def collect_data(team, pos):
-    base_url = "https://www.fangraphs.com/leaders/minor-league?pos={pos}&level=0&lg=2,4,5,6,7,8,9,10,11,14,12,13,15,16,17,18,30,32&stats=bat&qual=50&type=0&team={team}&season=2025&seasonEnd=2025&org=15&ind=0&splitTeam=false&players=&sort=23,1&pg=0";
+org_to_id = {
+    "los-angeles-angels": 1,
+    'baltimore-orioles': 2,
+    'boston-red-sox': 3,
+    'chicago-white-sox': 4,
+    'cleveland-guardians': 5,
+    'detroit-tigers': 6,
+    'kansas-city-royals': 7,
+    'minnesota-twins': 8,
+    'new-york-yankees': 9,
+    "the-athletics": 10,
+    'seattle-mariners': 11,
+    'tampa-bay-rays': 12,
+    'texas-rangers': 13,
+    'toronto-blue-jays': 14,
+    'arizona-diamondbacks': 15,
+    'atlanta-braves': 16,
+    'chicago-cubs': 17,
+    'cincinatti-reds': 18,
+    'colorado-rockies': 19,
+    'miami-marlins': 20,
+    "houston-astros": 21,
+    'los-angeles-dodgers': 22,
+    'milwaukee-brewers': 23,
+    'washington-nationals': 24,
+    'new-york-mets': 25,
+    'philidelphia-philies': 26,
+    'pittsburgh-pirates': 27,
+    'st-louis-cardinals': 28,
+    'san-diego-padres': 29,
+    'san-francisco-gians': 30,
+}
 
+def collect_data(team, pos):
+    base_url = f"https://www.fangraphs.com/leaders/minor-league?pos={pos}&level=0&lg=2,4,5,6,7,8,9,10,11,14,12,13,15,16,17,18,30,32&stats=bat&qual=50&type=0&team=&season=2025&seasonEnd=2025&org={org_to_id[team]}&ind=0&splitTeam=false&players=&sort=23,1"
+    
     response = requests.get(base_url)
     response.raise_for_status()
     html = response.text
@@ -49,8 +82,10 @@ def collect_data(team, pos):
     df = pd.DataFrame(rows, columns=headers)
     print(df.head())
 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python scrapeFangraphs.py [location-team] [position]")
+        sys.exit(1)
 
-collect_data("all")
-
-
+    collect_data(sys.argv[1], sys.argv[2])
 
